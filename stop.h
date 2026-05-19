8d0968b80cbc6766640dd87fe8056c5693492484
Damir <damirmatematic@gmail.com>
2026-05-19 17:26:13 +0300

    Added function of time of arrive

--- stop.h
+++ stop.h
@@ -1,19 +1,38 @@
 #include <string>
+#include <iostream>
 
 class Stop{
     public:
       std::string name;
-      int coord=0,time=0  ;
+      int coord_x=0,time=0,coord_y=0  ;
       Stop* next_stop = nullptr, *prev_stop = nullptr;
       Stop() = default;
-      Stop(std::string name,int coord,int time,Stop* previous):name(name),coord(coord),time(time),prev_stop(previous){}
-      Stop(std::string name,int coord,int time):name(name),coord(coord),time(time){}
+      Stop(std::string name,int coord_x,int coord_y,int time):name(name),coord_x(coord_x),coord_y(coord_y),time(time){}
 };
 
-class Stoplist{
+class StopList{
+    private:
+      Stop* end = nullptr;
+      Stop* start = nullptr;
     public:
-      Stoplist() = default;
-      void addstop(std::string name,int coord,int time){
-        Stop* new_stop = new Stop(name,coord,time);
+      StopList() = default;
+
+      void addstop(std::string name,int coord_x,int coord_y,int time);
+      Stop* After_N(int n);
+      int Time();
+      void Forw_Route();
+      void Back_Route();
+      void Cycle();
+      void ByTheTime(int time);
+      void After_N_Extended(int n);
+      ~StopList(){
+        end->next_stop = nullptr;
+        Stop* current = start;
+        while(current != nullptr){
+          Stop* next = current->next_stop;
+          delete current;
+          current = next;
+        }
       }
+      
 };
