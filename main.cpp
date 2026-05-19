d697524f9f604379e3231c0727aa7ec525f353d7
Damir <damirmatematic@gmail.com>
2026-05-19 14:12:33 +0300

    Created StopList

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "stop.h"
-
-int main(){
-    Stop start,last=start;
-
-}
--- stop.h
+++ stop.h
@@ -7,4 +7,13 @@ class Stop{
       Stop* next_stop = nullptr, *prev_stop = nullptr;
       Stop() = default;
       Stop(std::string name,int coord,int time,Stop* previous):name(name),coord(coord),time(time),prev_stop(previous){}
+      Stop(std::string name,int coord,int time):name(name),coord(coord),time(time){}
+};
+
+class Stoplist{
+    public:
+      Stoplist() = default;
+      void addstop(std::string name,int coord,int time){
+        Stop* new_stop = new Stop(name,coord,time);
+      }
 };
