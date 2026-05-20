5f138f1ae722c1e0aaafeae1195328fac7bd1455
Damir <damirmatematic@gmail.com>
2026-05-21 01:55:57 +0300

    Added '\n' in create_line

---
+++ base64.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include "stop.hpp"
+
+std::string create_line(Stop* current){
+    std::string line = 
+                        current->name+" "+
+                        std::to_string(current->coord_x)+" "+
+                        std::to_string(current->coord_y)+" "+
+                        std::to_string(current->time);
+    line.push_back('\n');
+    return line;
+}
+
+const std::string Base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+std::string Encode(std::string& input){
+    std::string result;
+    int val=0,bits=-6;
+
+    for(unsigned char c:input){
+        val = (val<<8)+c;
+        bits+=8;
+        while(bits>=0){
+            result.push_back(Base64[(val >> bits) & 0x3F]);
+            bits -= 6;
+        }
+    }
+    if (bits > -6)
+    {
+        result.push_back(Base64[((val << 8) >> (bits + 8)) & 0x3F]);
+    }
+
+    while (result.size() % 4)
+    {
+        result.push_back('=');
+    }
+    return result;
+}
